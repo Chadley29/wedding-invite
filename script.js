6b0deefcbc1bffb3e62f50e5aafacd6953512120
chadleybissolati <123072320+Chadley29@users.noreply.github.com>
2023-07-05 16:50:26 +0200

    hamburger menu addition

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+// HamburgerMenu
+const buttons = document.querySelectorAll(".hamburger-button");
+const mobileNav = document.querySelector(".primary-header__nav");
+
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentState = button.getAttribute("data-state");
+
+    if (!currentState || currentState === "closed") {
+      button.setAttribute("data-state", "opened");
+      button.setAttribute("aria-expanded", "true");
+      mobileNav.style.display = "block";
+    } else {
+      button.setAttribute("data-state", "closed");
+      button.setAttribute("aria-expanded", "false");
+      mobileNav.style.display = "none";
+    }
+  });
+});
+
 // tingle
 var modal = new tingle.modal({
   footer: true,
