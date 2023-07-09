a7aa3c6a95d743e192b7ccb2ce15808593fe930f
chadleybissolati <123072320+Chadley29@users.noreply.github.com>
2023-07-09 15:49:58 +0200

    registry modal

--- script.js
+++ script.js
@@ -17,33 +17,3 @@ buttons.forEach((button) => {
     }
   });
 });
-
-// tingle
-var modal = new tingle.modal({
-  footer: true,
-  stickyFooter: false,
-  closeMethods: ["overlay", "button", "escape"],
-  closeLabel: "Close",
-  cssClass: ["custom-class-1", "custom-class-2"],
-  onOpen: function () {
-    console.log("modal open");
-  },
-  onClose: function () {
-    console.log("modal closed");
-  },
-  beforeClose: function () {
-    // here's goes some logic
-    // e.g. save content before closing the modal
-    return true; // close the modal
-    return false; // nothing happens
-  },
-});
-
-// set content
-modal.setContent("<h1>here's some content</h1>");
-
-// open modal
-modal.open();
-
-// close modal
-modal.close();
